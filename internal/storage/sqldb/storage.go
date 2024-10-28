75138e5e177a32e11be3cd4efc2d43738872fa17
KonstantinSavin <110243772+KonstantinSavin@users.noreply.github.com>
2024-10-28 14:47:26 +0300

    fix readmy

--- internal/storage/sqldb/storage.go
+++ internal/storage/sqldb/storage.go
@@ -6,11 +6,13 @@ import (
 	"effective-mobile/music-lib/pkg/logging"
 
 	_ "github.com/lib/pq"
+	"github.com/sirupsen/logrus"
 )
 
 type Storage struct {
 	db      *sql.DB
 	songRep *SongRep
+	logger  *logrus.Logger
 }
 
 func New(db *sql.DB) *Storage {
